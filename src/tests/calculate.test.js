19256e27884337bd0a0af8f1f1984ed0307a8556
Denio Nhanale <dnhanale@gmail.com>
2023-06-08 15:10:18 +0200

    Fix Linters

--- src/tests/calculate.test.js
+++ src/tests/calculate.test.js
@@ -1,12 +1,12 @@
-import calculate from "../logic/calculate";
+import calculate from '../logic/calculate';
 
 describe('test for calculate js file', () => {
-    test('when AC button is clicked', () => {
-      const output = calculate({
-        total: '6',
-        next: '2',
-        operation: '',
-      }, 'AC');
-      expect(output.total).toEqual(null);
-    });
+  test('when AC button is clicked', () => {
+    const output = calculate({
+      total: '6',
+      next: '2',
+      operation: '',
+    }, 'AC');
+    expect(output.total).toEqual(null);
   });
+});
