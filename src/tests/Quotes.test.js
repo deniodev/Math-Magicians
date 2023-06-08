a42c34bc7a81e24c397989578e6f79b5d1bae695
Denio Nhanale <dnhanale@gmail.com>
2023-06-08 15:06:49 +0200

    Adding tests

---
+++ src/tests/Quotes.test.js
@@ -0,0 +1,11 @@
+import renderer from 'react-test-renderer';
+import Fetchquote from '../components/Quotes';
+
+describe('test if quote matches snapshot', () => {
+    test('test if quote is a match', () => {
+      const quoteComponent = renderer
+        .create(<Fetchquote />)
+        .toJSON();
+      expect(quoteComponent).toMatchSnapshot();
+    });
+  });
